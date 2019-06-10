90c655b8e13b1cc9b02d85419bcbf8ba7df69425
Tobias <tobias@appysphere.com>
2019-06-10 20:55:03 +0700

    Relations 2 directions

--- basics/src/index.js
+++ basics/src/index.js
@@ -3,20 +3,54 @@ import { GraphQLServer } from "graphql-yoga";
 // scalar types:
 //      String, Boolean, Int, Float, ID
 const users = [
+  { id: "1", name: "Mui", email: "mui@lu.com", age: 11 },
+  { id: "2", name: "Tobi", email: "tobi@lu.com", age: 12 },
+  { id: "3", name: "Tui", email: "tui@lu.com", age: 13 }
+];
+
+const queues = [
   {
-    id: "la",
-    name: "lu",
-    email: "la@lu.com",
-    age: 1
+    id: "1",
+    title: "Hospital Medication",
+    processed: false,
+    how_many_before: 0,
+    user: "3"
   },
-  { id: "la", name: "lu", email: "la@lu.com", age: 1 },
-  { id: "la", name: "lu", email: "la@lu.com", age: 1 }
+  {
+    id: "2",
+    title: "Hospital Medication",
+    processed: false,
+    how_many_before: 1,
+    user: "1"
+  },
+  {
+    id: "3",
+    title: "Hospital Medication",
+    processed: false,
+    how_many_before: 2,
+    user: "2"
+  },
+  {
+    id: "4",
+    title: "Hospital Doctor Appointment",
+    processed: false,
+    how_many_before: 0,
+    user: "2"
+  },
+  {
+    id: "5",
+    title: "Hospital Doctor Appointment",
+    processed: false,
+    how_many_before: 1,
+    user: "3"
+  }
 ];
 
 // type definitions ( schema )
 const typeDefs = ` 
     type Query {
         users(query: String): [User!]!
+        queue(query: ID!): [Queue!]!
         me: User!
     }
 
@@ -25,6 +59,15 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        queues: [Queue!]!
+    }
+
+    type Queue {
+      id: ID!
+      title: String!
+      processed: Boolean!
+      how_many_before: Int!
+      user: User!
     }
 `;
 
@@ -33,8 +76,10 @@ const resolvers = {
   Query: {
     users(parent, args) {
       if (!args.query) return users;
-
       return users.filter(u => u.name.toLowerCase().includes(args.query));
+    },
+    queue(parent, args) {
+      return queues.filter(q => q.id === args.query);
     },
     me() {
       return {
@@ -42,6 +87,16 @@ const resolvers = {
         name: "Tobsn",
         email: "caruso33@web.de"
       };
+    }
+  },
+  Queue: {
+    user(parent, args, ctx, info) {
+      return users.find(u => u.id === parent.user);
+    }
+  },
+  User: {
+    queues(parent, args, ctx, info) {
+      return queues.filter(q => q.user === parent.id);
     }
   }
 };
