1242093320c88ea3bfb463f1120487532b7ecbbc
Tobias <tobias@appysphere.com>
2019-06-10 21:20:11 +0700

    Relations 3 types

--- basics/src/index.js
+++ basics/src/index.js
@@ -3,45 +3,72 @@ import { GraphQLServer } from "graphql-yoga";
 // scalar types:
 //      String, Boolean, Int, Float, ID
 const users = [
-  { id: "1", name: "Mui", email: "mui@lu.com", age: 11 },
-  { id: "2", name: "Tobi", email: "tobi@lu.com", age: 12 },
-  { id: "3", name: "Tui", email: "tui@lu.com", age: 13 }
+  { id: "1", name: "Mui", email: "mui@lu.com", age: 11, comments: ["9"] },
+  { id: "2", name: "Tobi", email: "tobi@lu.com", age: 12, comments: [] },
+  { id: "3", name: "Tui", email: "tui@lu.com", age: 13, comments: ["10", "11"] }
 ];
 
 const queues = [
   {
-    id: "1",
+    id: "4",
     title: "Hospital Medication",
     processed: false,
     how_many_before: 0,
     user: "3"
   },
   {
-    id: "2",
+    id: "5",
     title: "Hospital Medication",
     processed: false,
     how_many_before: 1,
-    user: "1"
+    user: "1",
+    comment: "9"
   },
   {
-    id: "3",
+    id: "6",
     title: "Hospital Medication",
     processed: false,
     how_many_before: 2,
     user: "2"
   },
   {
-    id: "4",
+    id: "7",
     title: "Hospital Doctor Appointment",
     processed: false,
     how_many_before: 0,
-    user: "2"
+    user: "2",
+    comment: "10"
   },
   {
-    id: "5",
+    id: "8",
     title: "Hospital Doctor Appointment",
     processed: false,
     how_many_before: 1,
+    user: "3",
+    comment: "11"
+  }
+];
+
+const comments = [
+  {
+    id: "9",
+    title: "Hilarious!",
+    body: "Good fast service, highly appreciated",
+    queue: "5",
+    user: "1"
+  },
+  {
+    id: "10",
+    title: "Just Bad!",
+    body: "Unfriendly, long waiting",
+    queue: "7",
+    user: "3"
+  },
+  {
+    id: "11",
+    title: "Best hospital in town!",
+    body: "Love it every time I go there",
+    queue: "8",
     user: "3"
   }
 ];
@@ -51,6 +78,7 @@ const typeDefs = `
     type Query {
         users(query: String): [User!]!
         queue(query: ID!): [Queue!]!
+        comment(query: ID): [Comment!]!
         me: User!
     }
 
@@ -60,6 +88,7 @@ const typeDefs = `
         email: String!
         age: Int
         queues: [Queue!]!
+        comments: [Comment]!
     }
 
     type Queue {
@@ -67,6 +96,15 @@ const typeDefs = `
       title: String!
       processed: Boolean!
       how_many_before: Int!
+      user: User!
+      comment: Comment
+    }
+
+    type Comment {
+      id: ID!
+      title: String!
+      body: String!
+      queue: Queue!
       user: User!
     }
 `;
@@ -79,7 +117,12 @@ const resolvers = {
       return users.filter(u => u.name.toLowerCase().includes(args.query));
     },
     queue(parent, args) {
-      return queues.filter(q => q.id === args.query);
+      return queues.filter(q => q.title === args.query);
+    },
+    comment(p, args) {
+      return args.query
+        ? comments.filter(c => c.title === args.query)
+        : comments;
     },
     me() {
       return {
@@ -92,11 +135,25 @@ const resolvers = {
   Queue: {
     user(parent, args, ctx, info) {
       return users.find(u => u.id === parent.user);
+    },
+    comment(parent) {
+      return comments.find(c => c.id === parent.comment);
     }
   },
   User: {
     queues(parent, args, ctx, info) {
       return queues.filter(q => q.user === parent.id);
+    },
+    comments(parent) {
+      return comments.filter(c => parent.comments.includes(c.id));
+    }
+  },
+  Comment: {
+    queue(parent) {
+      return queues.find(q => q.id === parent.queue);
+    },
+    user(parent) {
+      return users.find(u => u.id === parent.user);
     }
   }
 };
