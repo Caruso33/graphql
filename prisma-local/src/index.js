38f795fc9cf3f2ddf4dd230a9879a635b691e638
Tobias <tobias@appysphere.com>
2019-06-14 08:05:43 +0700

    Import prisma client properly

--- prisma-local/src/index.js
+++ prisma-local/src/index.js
@@ -6,16 +6,22 @@ import Query from "./resolvers/Query";
 import Queue from "./resolvers/Queue";
 import Subscription from "./resolvers/Subscription";
 import User from "./resolvers/User";
-import "./prisma";
+import { prisma } from './generated/prisma-client'
 
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: { Query, User, Comment, Subscription, Mutation, Queue },
-  context: { db, pubsub }
+  context: { db, pubsub, prisma }
 });
 
 server.start(() => {
   console.log("server is up");
+
+console.log(prisma.mutation)
 });
+
+// prisma.users(null, "{ id name email }").then(data => {
+//   console.log(data);
+// });
--- prisma-local/src/prisma.js
+++
@@ -1,9 +0,0 @@
-import { Prisma } from './generated/prisma-client'
-
-const prisma = new Prisma({
-  endpoint: "http://localhost:4466"
-});
-
-prisma.users(null, "{ id name email }").then(data => {
-  console.log(data);
-});
