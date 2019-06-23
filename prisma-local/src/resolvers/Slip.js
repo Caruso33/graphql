2809200cec7c26c7947fbfc1f0d58665d83f8e00
Tobias <tobias@appysphere.com>
2019-06-23 16:56:29 +0700

    Introduce fragments

--- prisma-local/src/resolvers/Slip.js
+++ prisma-local/src/resolvers/Slip.js
@@ -1,6 +1,24 @@
+import getUserId from "../utils/getUserId"
+
 const Slip = {
-  how_many_before() {
-    return 0
+  how_many_before: {
+    fragment: "fragment slipId on Slip { id }",
+    async resolve(parent, { id }, { prisma, request }, info) {
+      const userId = getUserId(request)
+
+      // const queue = await prisma.bindings.query.queues(
+      //   {
+      //     where: {
+      //       slips: { slips_some: { id: parent.queue } }
+      //     }
+      //   },
+      //   info
+      // )
+
+      // const index = queue.slips.indexOf(id) + 1
+      // console.log("index", queue)
+      return 0
+    }
   }
 }
 
--- prisma-local/src/resolvers/User.js
+++ prisma-local/src/resolvers/User.js
@@ -1,5 +1,15 @@
+import getUserId from "../utils/getUserId"
+
 const User = {
+  email: {
+    fragment: "fragment userId on User { id }",
+    resolve(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request, false)
 
-};
+      if (userId && userId === parent.id) return parent.email
+      else return null
+    }
+  }
+}
 
-export default User;
+export default User
