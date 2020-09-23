f696325ae75db9365dc81a9b0463647c0e560443
Tobias Leinss <caruso33@web.de>
2020-09-23 17:15:06 +0200

    Implement crud

--- 6_next_graphql/src/resolvers/queue.ts
+++ 6_next_graphql/src/resolvers/queue.ts
@@ -1,11 +1,60 @@
 import { MyContext } from "./../types"
 import { Queue } from "./../entities/Queue"
-import { Resolver, Query, Ctx } from "type-graphql"
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql"
 
 @Resolver()
 export class QueueResolver {
   @Query(() => [Queue])
   queues(@Ctx() { em }: MyContext): Promise<Queue[]> {
     return em.find(Queue, {})
+  }
+
+  @Query(() => Queue, { nullable: true })
+  queue(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Queue | null> {
+    return em.findOne(Queue, { id })
+  }
+
+  @Mutation(() => Queue)
+  async createQueue(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Queue> {
+    const queue = em.create(Queue, { title })
+    await em.persistAndFlush(queue)
+    return queue
+  }
+
+  @Mutation(() => Queue, { nullable: true })
+  async updateQueue(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string | null,
+    @Ctx() { em }: MyContext
+  ): Promise<Queue | null> {
+    const queue = await em.findOne(Queue, { id })
+    if (!queue) {
+      return null
+    }
+
+    if (typeof title !== "undefined") queue.title = title!
+
+    await em.persistAndFlush(queue)
+
+    return queue
+  }
+
+  @Mutation(() => Boolean)
+  async deleteQueue(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    try {
+      await em.nativeDelete(Queue, { id })
+      return true
+    } catch {
+      return false
+    }
   }
 }
